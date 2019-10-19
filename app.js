0815ed6bbac9f7145c14fc1c929c108d062c7fe4
TLauxmann <tobias_holger.lauxmann@student.reutlingen-university.de>
2019-10-19 13:48:35 +0200

    added userOnline List

--- app.js
+++ app.js
@@ -1,23 +1,43 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.sendFile(__dirname + '/client/index.html');
 });
 
-http.listen(3000, function () {
+app.use('/client', express.static(__dirname + '/client'));
+
+http.listen(3000, function() {
     console.log('listening on *:3000');
 });
 
-io.on('connection', function (socket) {
-    console.log('a user connected');
+var usersOnline = []
 
-    socket.on('disconnect', function () {
-        console.log('user disconnected');
+io.on('connection', function(socket) {
+
+    socket.on('disconnect', function() {
+        usersOnline.splice(usersOnline.indexOf(socket.username), 1);
+        socket.broadcast.emit('userLeft', socket.username) // to all others
     });
 
-    socket.on('chat message', function (msg) {
+    socket.on('chat message', function(msg) {
         io.emit('chat message', msg);
     });
+
+    //user Login
+    socket.on('checkName', function(username) {
+        if (!usersOnline.includes(username)) {
+
+            socket.username = username;
+            usersOnline.push(username);
+
+            socket.emit('validLogin', usersOnline);
+            socket.broadcast.emit('userJoint', username) // to all others
+        } else {
+            socket.emit('invalidLogin');
+        }
+    });
+
 });
--- client/script.js
+++ client/script.js
@@ -1,1 +1,50 @@
-script
+$(document).ready(function() {
+
+    $(function() {
+
+        var socket = io();
+
+        //enter message
+        $('form').submit(function(e) {
+
+            e.preventDefault(); // prevents page reloading
+            socket.emit('chat message', $('#m').val());
+            $('#m').val('');
+            return false;
+        });
+
+        //recieve message
+        socket.on('chat message', function(msg) {
+            $('#messages').append($('<li>').text(msg));
+        });
+
+        //checks if name already exists
+        $('#submitName').click(function() {
+            if ($("#username").val()) {
+                socket.emit('checkName', $("#username").val());
+            }
+        });
+
+        socket.on('validLogin', function(usersOnline) {
+            usersOnline.forEach(function(username) {
+                $("#onlineList").append("<li id=" + username + ">" + username + "</li>");
+            });
+            $("#login").hide();
+            $("#mainChat").show();
+        });
+
+        socket.on('invalidLogin', function() {
+            alert("This username already exists!")
+        });
+
+        socket.on('userJoint', function(username) {
+            $("#onlineList").append("<li id=" + username + ">" + username + "</li>");
+        });
+
+        socket.on('userLeft', function(username) {
+            $("#" + username).remove();
+        });
+
+
+    });
+});
