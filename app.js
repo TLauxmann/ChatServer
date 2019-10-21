6127fb5e245b60b414b30488fb1554dd6e9a473f
TLauxmann <tobias_holger.lauxmann@student.reutlingen-university.de>
2019-10-21 12:35:17 +0200

    timestamp for user entering, leaving chat

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ io.on('connection', function(socket) {
     });
 
     socket.on('chat message', function(msg) {
-        io.emit('chat message', msg);
+        io.emit('chat message', msg, socket.username);
     });
 
     //user Login
--- client/script.js
+++ client/script.js
@@ -1,49 +1,55 @@
 $(document).ready(function() {
 
-    $(function() {
+    var socket = io();
 
-        var socket = io();
+    //enter message
+    $('form').submit(function(e) {
+        e.preventDefault(); // prevents page reloading
+        socket.emit('chat message', $('#m').val());
+        $('#m').val('');
+        return false;
+    });
 
-        //enter message
-        $('form').submit(function(e) {
-            e.preventDefault(); // prevents page reloading
-            socket.emit('chat message', $('#m').val());
-            $('#m').val('');
-            return false;
-        });
+    //recieve message
+    socket.on('chat message', function(msg, username) {
+        $('#messages').append($('<li>').text(getCurrentTimestamp() + " " + username + ": " + msg));
+    });
 
-        //recieve message
-        socket.on('chat message', function(msg) {
-            $('#messages').append($('<li>').text(msg));
-        });
+    //checks if name already exists
+    $('#submitName').click(function() {
+        if ($("#username").val()) {
+            socket.emit('checkName', $("#username").val());
+        }
+    });
 
-        //checks if name already exists
-        $('#submitName').click(function() {
-            if ($("#username").val()) {
-                socket.emit('checkName', $("#username").val());
-            }
-        });
-
-        socket.on('validLogin', function(usersOnline) {
-            usersOnline.forEach(function(username) {
-                $("#onlineList").append(`<li id= ${username} ><button> ${username} </button></li>`);
-            });
-            $("#login").hide();
-            $("#mainChat").show();
-        });
-
-        socket.on('invalidLogin', function() {
-            alert("This username already exists!")
-        });
-
-        socket.on('userJoint', function(username) {
+    socket.on('validLogin', function(usersOnline) {
+        usersOnline.forEach(function(username) {
             $("#onlineList").append(`<li id= ${username} ><button> ${username} </button></li>`);
         });
-
-        socket.on('userLeft', function(username) {
-            $("#" + username).remove();
-        });
-
-
+        $("#login").hide();
+        $("#mainChat").show();
     });
+
+    socket.on('invalidLogin', function() {
+        alert("This username already exists!")
+    });
+
+    socket.on('userJoint', function(username) {
+        $("#onlineList").append(`<li id= ${username} ><button> ${username} </button></li>`);
+        $('#messages').append($('<li>').text(getCurrentTimestamp() + " " + username + " joint the chatroom"));
+    });
+
+    socket.on('userLeft', function(username) {
+        $("#" + username).remove();
+        $('#messages').append($('<li>').text(getCurrentTimestamp() + " " + username + " left the chatroom"));
+    });
+
 });
+
+function getCurrentTimestamp() {
+    var today = new Date();
+    var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date + ' ' + time;
+    return dateTime;
+}
