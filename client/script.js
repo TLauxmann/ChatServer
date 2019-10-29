151d42b7904bfc8ba827846e9d4fb360d451e669
TLauxmann <tobias_holger.lauxmann@student.reutlingen-university.de>
2019-10-29 17:52:40 +0100

    check input, removed comments

--- client/script.js
+++ client/script.js
@@ -5,22 +5,21 @@ $(document).ready(function() {
     //enter message and/or upload file
     $('form').submit(function(e) {
         e.preventDefault();
-        const writingToList = [];
+
+        if(checkInputForTags($('#m').val())) return;
+
+        var writingToList = [];
         $('.chosen').each(function(index) {
             writingToList.push($(this).attr("id"));
-            console.log($(this).attr("id").value);
-            console.log(writingToList.length);
-            console.log(JSON.stringify(writingToList));
         });
-       /* $('.chosen').each(function (index) {
-            writingToList.push($(this))
-        });*/
+
         const files = document.getElementById('file').files;
         //check if message has file attached
         if (files.length > 0) {
             const reader = new FileReader();
             reader.readAsDataURL(files[0]);
             reader.onload = function () {
+                if (checkInputForTags(reader.result)) return;
                 socket.emit('chat message', $('#m').val(), reader.result, writingToList);
                 $('#m').val('');
                 $('#file').val('');
@@ -29,24 +28,18 @@ $(document).ready(function() {
                 console.log('Error: ', error);
             };
         } else {
+            if ($('#m').val().length === 0) return;
             socket.emit('chat message', $('#m').val(),'', writingToList);
-            console.log(writingToList.length);
             $('#m').val('');
         }
     });
 
     //recieve message and check for file   
     socket.on('chat message', function (msg, username, file, sendTo) {
-
         //create String for tooltip
         var sendToList = '<br><div class= "tooltip">sent to<span class= "tooltiptext">';
-        console.log("length: " + sendTo.length);
-        console.log(JSON.stringify(sendTo));
         if (sendTo.length) {
             sendTo.forEach(function (chatpartner) {
-                console.log(sendTo.length);
-                /*console.log(chatpartner.value);
-                console.log(chatpartner.data);*/
                 sendToList = sendToList.concat(chatpartner + "<br>");
             });
             sendToList = sendToList.concat("</span ></div>");
@@ -63,15 +56,9 @@ $(document).ready(function() {
                 appendMedia(username, file, msg, "video", sendToList);
             } else if (fileType == "audio") {
                 appendMedia(username, file, msg, "audio", sendToList);
-            }/* else if ((file.split(": ",2)[1]).substr(5,4) == "text") {
-                var decodedText = b64DecodeUnicode((file.split(": ",2)[1]).split(";base64,",2)[1]);
-                $('#messages').append('<li> <p>' + file.split(": ",2)[0] + ' has sent a text file: ' + decodedText + '</p>');
-            } else if ((file.split(": ",2)[1]).substr(5,11) == "application") {
-                if ((file.split(": ",2)[1]).substr(17,12) == "octet-stream") {
-                    var decodedText = b64DecodeUnicode((file.split(": ",2)[1]).split(";base64,",2)[1]);
-                    $('#messages').append('<li> <p>' + file.split(": ",2)[0] + ' has sent a text file: ' + decodedText + '</p>');
-                }
-            }*/
+            } else{
+                $('#messages').append(`<li> ${getCurrentTimestamp()} ${username}: MEDIATYPE NOT SUPPORTED! ${msg} ${sendToList} </li>`);
+            }
         } else {
             $('#messages').append(`<li> ${getCurrentTimestamp()} ${username}: ${msg} ${sendToList} </li>`);
         }
@@ -79,7 +66,11 @@ $(document).ready(function() {
     
     //checks if name already exists
     $('#submitName').click(function() {
-        if ($("#username").val()) {
+        if ($("#username").val().length === 0){
+            alert("No empty username!");
+        } else if ($("#username").val().length > 20){
+            alert("This username is too long!")
+        } else {
             socket.emit('checkName', $("#username").val());
         }
     });
@@ -98,24 +89,14 @@ $(document).ready(function() {
 
     socket.on('userJoint', function(username) {
         $("#onlineList").append(`<li class=${username} ><button onclick="addToWritingList('${username}')" > ${username} </button></li>`);
-        $('#messages').append($('<li>').text(getCurrentTimestamp() + " " + username + " joint the chatroom"));
+        $('#messages').append($('<li class=userJointLeft >').text(getCurrentTimestamp() + " " + username + " joint the chatroom"));
     });
 
     socket.on('userLeft', function(username) {
         var listClass = $("." + username);
         listClass[0].remove();
-        if(listClass[1]){
-            listClass[1].remove();
-        }
-        $('#messages').append($('<li>').text(getCurrentTimestamp() + " " + username + " left the chatroom"));
+        $('#messages').append($('<li class=userJointLeft >').text(getCurrentTimestamp() + " " + username + " left the chatroom"));
     });
-      
-    function b64DecodeUnicode(str) {
-        // Going backwards: from bytestream, to percent-encoding, to original string.
-        return decodeURIComponent(atob(str).split('').map(function(c) {
-            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-        }).join(''));
-    }
 
 });
 
@@ -132,9 +113,6 @@ function getCurrentTimestamp() {
 }
 
 function addToWritingList(username){
-   /* if (!$("#" + username).length){
-        $("#writingToList").append(`<li id=${username} class=${username} ><button onclick="deleteElementById('${username}')" > ${username} </button></li>`);
-    } */
     var button = $("." + username + " button");
     if (button.hasClass('chosen')) {
         button.css('background-color', 'lightgrey');
@@ -149,4 +127,9 @@ function addToWritingList(username){
 
 function deleteElementById(id){
     $("#" + id).remove();
+}
+
+function checkInputForTags(input){
+    var regularExp = /(<script(\s|\S)*?<\/script>)|(<style(\s|\S)*?<\/style>)|(<!--(\s|\S)*?-->)|(<\/?(\s|\S)*?>)/g;
+    return regularExp.test(input);
 }
